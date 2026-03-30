f36f4f8d3f85e8230a1ebe537bcfb4f9d06805a7
Mussab12 <shahidmussab7@gmail.com>
2026-03-31 02:56:08 +0500

    making home section mobile first responsive

---
+++ MuuqWear.Web/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿window.focusElement = (element) => {
+    if (element) {
+        element.focus();
+    }
+};
+
+window.disableScroll = () => {
+    document.body.classList.add("no-scroll");
+};
+
+window.enableScroll = () => {
+    document.body.classList.remove("no-scroll");
+};
